20321d9f67af898e306625bd379ee4f742b38a2f
apifenylabs-2612 <apifenylabs@gmail.com>
2026-05-19 05:14:02 +0800

    feat: add Vercel Analytics + GA4 to affiliate-tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,11 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+        <GoogleAnalytics />
+      </body>
     </html>
   );
 }
